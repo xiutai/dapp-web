5fd5906dbb3817f12a58c3c1d0ae76abf8755598
xiutai <xiutai666@outlook.com>
2022-11-28 23:48:20 +0800

    xiutai

---
+++ assets/Tp.53d8eb64.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./index.4f291294.js";const c={};function e(t,_){return" 123123 "}const o=r(c,[["render",e]]);export{o as default};
--- assets/Tp.c18f8f54.js
+++
@@ -1,5 +0,0 @@
-import{_ as t,o as e,f as o,b as n}from"./index.3c40d222.js";const s={},l={align:"center"},c=n("br",null,null,-1),r=n("br",null,null,-1),_=n("br",null,null,-1),a=n("a",{href:`tpdapp://open?params={\r
-            "url": "https://www.scanadmin.com/#/test",\r
-            "chain": "tron",\r
-            "source": "xxx" \r
-        }`},"\u62C9\u8D77tp\u94B1\u5305",-1),i=n("br",null,null,-1),d=n("br",null,null,-1),u=n("br",null,null,-1),h=n("a",{href:"imtokenv2://navigate/DappView?url=https%3A%2F%2Fwww.scanadmin.com%2F%23%2Ftest"},"\u6253\u5F00 imToken",-1),p=[c,r,_,a,i,d,u,h];function m(f,b){return e(),o("div",l,p)}const x=t(s,[["render",m]]);export{x as default};
